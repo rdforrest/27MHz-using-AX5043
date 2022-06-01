5f901b74ae5c97d2a60bd523cd61e7188ed7cbaa
David Forrest <33370970+rdforrest@users.noreply.github.com>
2022-06-01 08:40:18 +0100

    Add files via upload

---
+++ ax5043-Jonathan-2.5/ax5043-Jonathan-2.5.ino
@@ -0,0 +1,21 @@
+
+
+// ***********************************************************************************************
+//  Name:      Basic write test
+//  Date:       16 May 2022
+//  Brief:       Crude SPI test - read write
+//  Author:     Jonathan Askey
+// ***********************************************************************************************
+
+// RDF put in configuration values from : https://www.notblackmagic.com/bitsnpieces/ax5043/
+// For 16 Mhz Crystal, RF 436.45 MHz. Power 12dBm
+
+/*  SPI connections
+    Uno/Nano      AX5043
+    MOSI  11      17
+    MISO  12      16
+    SCK   13      15
+    SS    10      14
+    3.3v          23
+    GND            6
+*/
