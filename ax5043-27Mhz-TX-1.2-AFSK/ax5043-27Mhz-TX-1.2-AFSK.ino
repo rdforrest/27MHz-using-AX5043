31b2cf027e4009290e9d17d54f20b00d9f2b78e2
David Forrest <33370970+rdforrest@users.noreply.github.com>
2022-08-09 11:24:04 +0100

    Add files via upload

---
+++ ax5043-27Mhz-TX-1.2-AFSK/ax5043-27Mhz-TX-1.2-AFSK.ino
@@ -0,0 +1,35 @@
+
+
+// ***********************************************************************************************
+//  Name:      Basic read/write test
+//  Date:       16 May 2022
+//  Brief:       Crude SPI test - read write
+//  Author:     Jonathan Askey
+// ***********************************************************************************************
+
+// RDF put in configuration values from : https://www.notblackmagic.com/bitsnpieces/ax5043/
+// For 16 Mhz Crystal with 5 pF capacitance. RF 458.75 MHz. AFSK modulation. Power 16dBm. 
+
+/*  SPI connections
+    Uno/Nano      AX5043
+    MOSI  11      17
+    MISO  12      16
+    SCK   13      15
+    SS    10      14
+    3.3v          23
+    GND            6
+*/
+
+/*
+   (Startup routine from Data sheet)
+   Reset the Chip. Set SEL to high for at least 1μs, then low. Wait until MISO goes high.
+  Set, and then clear, the RST bit of register PWRMODE.
+  2. Set the PWRMODE register to POWERDOWN.
+  3. Program parameters. It is recommended that suitable parameters are calculated
+  using the AX_RadioLab tool available from Axsem.
+*/
+/*
+ * Current draw  as RX  0.7 mA - too low from data sheet
+ * 
+ *
+ */
